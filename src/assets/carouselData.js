ea0820cfa9e9dee76c6054a3e81e274f8bc2fe5c
Konstantinos Efkarpidis <kefkarpidis@deloitte.gr>
2022-07-09 15:16:25 +0300

    Added camera project

--- src/assets/carouselData.js
+++ src/assets/carouselData.js
@@ -1,6 +1,16 @@
 export const DATA = [
   {
     id: '1',
+    title: 'Camera App',
+    description:
+      'App that uses camera hardware through react-native-vision-camera library. There is a Preview screen to discard or upload your image/video and Gallery screen to open it!',
+    technologies: ['Javascript', 'React Native'],
+    url: 'https://github.com/kostas64/CameraApp/',
+    env: 'Mobile',
+    image: require('../assets/images/camera.png'),
+  },
+  {
+    id: '2',
     title: 'Demonstration App',
     description:
       'This project is the one you use right now. App demonstrates mechanisms of React Native and how i use them. Some of them is DrawerNavigation, Lotties, Animated API, AppState and Carousel.',
@@ -10,7 +20,7 @@ export const DATA = [
     image: require('../assets/images/demoDrawer.png'),
   },
   {
-    id: '2',
+    id: '3',
     title: 'Authentication App',
     description:
       'It is about best practices on authnetication flow. Navigation login/logout is based on token life.',
@@ -20,7 +30,7 @@ export const DATA = [
     image: require('../assets/images/authApp.png'),
   },
   {
-    id: '3',
+    id: '4',
     title: 'React SPA',
     description:
       'React single page application with Routing, Fetch API, Firebase for the database, Custom Hook for http requests.',
@@ -30,7 +40,7 @@ export const DATA = [
     image: require('../assets/images/guoteNew.png'),
   },
   {
-    id: '4',
+    id: '5',
     title: 'Food Order App',
     description:
       'It is a simple React app that contains Cart and meals that you can add or remove from there.',
@@ -40,7 +50,7 @@ export const DATA = [
     image: require('../assets/images/foodOrder.png'),
   },
   {
-    id: '5',
+    id: '6',
     title: 'MultiPage App',
     description:
       'React web app with Router v6 demonstration, consisting of 2 routes and a nested one.',
@@ -50,7 +60,7 @@ export const DATA = [
     image: require('../assets/images/multiMain.png'),
   },
   {
-    id: '6',
+    id: '7',
     title: 'Animations App',
     description:
       'Another web React app that demonstrates a modal, add and remove item and a box enabled/disabled by a toggler.',
